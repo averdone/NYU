528f302b24350ba67910efc34b26d7b70e508381
averdone <31397299+averdone@users.noreply.github.com>
2023-06-30 13:37:45 -0400

    Add files via upload

---
+++ Part1.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<HTML>
+<BODY>
+<H1><center> Our First JavaScript Example </center></H1>
+<BR>
+
+<h2>Writing using: document.write( )</h3>
+
+<SCRIPT>
+    document.write("<tr>
+    <th>1</th><th>2</th><th>3</th>
+    </tr>");
+</SCRIPT>
+
+
+</BODY>
+</HTML>
